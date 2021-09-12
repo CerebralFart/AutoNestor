36873787c535699687736d844258a2b2c1b03e23
Sven Mol <s.mol@student.utwente.nl>
2021-09-12 14:50:05 +0200

    Pivot task assignment table on dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,29 +9,59 @@
             <thead>
             <tr>
                 <td></td>
-                <td class="px-2">
-                    Vorige week<br/>
-                    <small>{{\App\Models\Week::current()->previous->label}}</small>
-                </td>
-                <td class="px-2">
-                    Deze week<br/>
-                    <small>{{\App\Models\Week::current()->label}}</small>
-                </td>
-                <td class="px-2">
-                    Volgende week<br/>
-                    <small>{{\App\Models\Week::current()->next->label}}</small>
-                </td>
+                @foreach(\App\Models\User::all() as $user)
+                    <td @class([
+                        "px-2",
+                        "bg-blue-100" => $user->id === auth()->id()
+                    ])>
+                        {{$user->name}}
+                    </td>
+                @endforeach
             </tr>
             </thead>
             <tbody>
-            @foreach(\App\Models\User::all() as $user)
-                <tr>
-                    <td>{{$user->name}}</td>
-                    <td class="px-2">{{$user->assignmentForWeek(\App\Models\Week::current()->previous)?->task->name ?? 'Geen taak'}}</td>
-                    <td class="px-2">{{$user->assignmentForWeek(\App\Models\Week::current())?->task->name ?? 'Geen taak'}}</td>
-                    <td class="px-2">{{$user->assignmentForWeek(\App\Models\Week::current()->next)?->task->name ?? 'Geen taak'}}</td>
-                </tr>
-            @endforeach
+            <tr>
+                <td class="pr-2">
+                    Vorige week<br/>
+                    <small class="font-sm">{{\App\Models\Week::current()->previous->label}}</small>
+                </td>
+                @foreach(\App\Models\User::all() as $user)
+                    <td @class([
+                        "p-2",
+                        "bg-blue-100" => $user->id === auth()->id()
+                    ])>
+                        {{$user->assignmentForWeek(\App\Models\Week::current()->previous)?->task->name ?? 'Geen taak'}}
+                    </td>
+                @endforeach
+            </tr>
+            <tr>
+                <td class="pr-2">
+                    Deze week<br/>
+                    <small class="font-sm">{{\App\Models\Week::current()->label}}</small>
+                </td>
+                @foreach(\App\Models\User::all() as $user)
+                    <td @class([
+                        "p-2",
+                        "bg-blue-100" => $user->id === auth()->id()
+                    ])>
+                        {{$user->assignmentForWeek(\App\Models\Week::current())?->task->name ?? 'Geen taak'}}
+                    </td>
+                @endforeach
+            </tr>
+            <tr>
+                <td class="pr-2">
+                    Volgende week<br/>
+                    <small class="font-sm">{{\App\Models\Week::current()->next->label}}</small>
+                </td>
+                @foreach(\App\Models\User::all() as $user)
+                    <td @class([
+                        "p-2",
+                        "bg-blue-100" => $user->id === auth()->id()
+                    ])>
+                        {{$user->assignmentForWeek(\App\Models\Week::current()->next)?->task->name ?? 'Geen taak'}}
+                    </td>
+                @endforeach
+            </tr>
             </tbody>
         </table>
     </div>
