cb1e872584b50986942f34654dbf3dfc11f2e053
Sven Mol <s.mol@student.utwente.nl>
2021-09-12 14:33:40 +0200

    Respect policies in list pages

--- resources/views/users/list.blade.php
+++ resources/views/users/list.blade.php
@@ -32,19 +32,23 @@
                             </div>
                         </div>
                         <div class="flex-grow flex flex-row gap-2 justify-end items-center">
-                            <a href="{{route('users.update', ['id' => $item->id])}}">
-                                <svg class="h-5 w-5 text-gray-400 hover:text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/>
-                                </svg>
-                            </a>
-                            <form method="POST" action="{{route('users.delete', ['id' => $item->id])}}" class="m-0">
-                                @csrf
-                                <button type="submit" class="h-5">
-                                    <svg class="h-5 w-5 text-gray-400 hover:text-red-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
+                            @can('update', $item)
+                                <a href="{{route('users.update', ['id' => $item->id])}}">
+                                    <svg class="h-5 w-5 text-gray-400 hover:text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/>
                                     </svg>
-                                </button>
-                            </form>
+                                </a>
+                            @endcan
+                            @can('delete',$item)
+                                <form method="POST" action="{{route('users.delete', ['id' => $item->id])}}" class="m-0">
+                                    @csrf
+                                    <button type="submit" class="h-5">
+                                        <svg class="h-5 w-5 text-gray-400 hover:text-red-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
+                                        </svg>
+                                    </button>
+                                </form>
+                            @endcan
                         </div>
                     </div>
                 </li>
