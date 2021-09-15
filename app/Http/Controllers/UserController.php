d4f0e7eb5a2abeb5076969be44c82e9df4f75b09
Sven Mol <s.mol@student.utwente.nl>
2021-09-15 12:33:24 +0200

    Add user update page

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,6 +12,8 @@ class UserController extends CRUDController {
     protected $model = User::class;
     protected $views = 'users';
 
+    // TODO: Ensure there is always one admin left after updates and deletes, otherwise throw an error
+
     public function delete(Request $request, string $id) {
         /** @var Model $instance */
         $instance = User::find($id);
--- resources/views/users/update.blade.php
+++ resources/views/users/update.blade.php
@@ -1,4 +1,45 @@
 @extends('layout.base')
 
 @section('title', 'Gebruiker \'' . $item->name . '\' bewerken')
-{{--TODO inhoud geven--}}
+
+@section('buttons')
+    @can('delete', $item)
+        <form method="POST" action="{{route('users.delete', ['id' => $item->id])}}" class="m-0">
+            @csrf
+            <x-button
+                color="red"
+                icon="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                type="submit"
+            >
+                Verwijderen
+            </x-button>
+        </form>
+    @endif
+@endsection
+
+@section('content')
+    <form class="flex flex-col gap-2 mt-4" method="POST">
+        <x-form.text
+            id="name"
+            name="Naam"
+            value="{{$item->name}}"
+        />
+        <x-form.text
+            id="email"
+            name="E-mailadres"
+            value="{{$item->email}}"
+        />
+        <x-form.select
+            id="role"
+            name="Rol"
+            :value="$item->role"
+            :options="['admin', 'user']"
+        />
+        @csrf
+        <div class="flex flex-row justify-end">
+            <x-button type="submit" color="green">
+                Opslaan
+            </x-button>
+        </div>
+    </form>
+@endsection
