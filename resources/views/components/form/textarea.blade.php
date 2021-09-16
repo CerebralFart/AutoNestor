222162ba1063185b8f1aeb55c72cc19e89d2d693
Sven Mol <s.mol@student.utwente.nl>
2021-09-16 15:30:53 +0200

    Respect value in textarea component

--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -10,6 +10,6 @@
             id="{{$attributes->get('id')}}"
             name="{{$attributes->get('id')}}"
             class="shadow-sm block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border border-gray-300 rounded-md"
-        ></textarea>
+        >{{$attributes->get('value', '')}}</textarea>
     </div>
 </div>
