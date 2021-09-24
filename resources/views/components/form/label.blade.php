88dbd4a28931c59bcc57d2fa01645de42877ea10
Sven Mol <s.mol@student.utwente.nl>
2021-09-24 20:40:11 +0200

    Fix double-escaping issue in form/label component

--- resources/views/components/form/label.blade.php
+++ resources/views/components/form/label.blade.php
@@ -1,6 +1,6 @@
 <label
-    for="{{$attributes->get('id')}}"
+    for="{!! $attributes->get('id') !!}"
     class="block font-medium text-gray-700"
 >
-    {{$attributes->get('name')}}:
+    {!! $attributes->get('name') !!}:
 </label>
