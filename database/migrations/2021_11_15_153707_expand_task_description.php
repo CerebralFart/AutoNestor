159dc7ec0c8282e1490748a66ad801dfe86a5599
Sven Mol <s.mol@student.utwente.nl>
2021-11-15 16:43:19 +0100

    Make Task#description longer

---
+++ database/migrations/2021_11_15_153707_expand_task_description.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ExpandTaskDescription extends Migration {
+    public function up() {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->text('description')->nullable()->change();
+        });
+    }
+
+    public function down() {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->string('description')->nullable()->change();
+        });
+    }
+}
