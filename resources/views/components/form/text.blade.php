58a32a38fd68d2d74919931cc33a74b4e592a75e
Sven Mol <s.mol@student.utwente.nl>
2021-11-15 16:43:37 +0100

    Bugfix in form.text component when rendering HTML entities

--- resources/views/components/form/text.blade.php
+++ resources/views/components/form/text.blade.php
@@ -11,7 +11,7 @@
             name="{{$attributes->get('id')}}"
             id="{{$attributes->get('id')}}"
             class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-            value="{{$attributes->get('value', '')}}"
+            value="{!! $attributes->get('value', '') !!}"
         />
     </div>
 </div>
