6d73d0f5a58cb6158b9bab2fd854ff0498d75c94
Sven Mol <s.mol@student.utwente.nl>
2021-12-25 15:08:23 +0100

    Clean up dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,14 @@
 @extends('layout.base')
 
+@php
+    use App\Models\Week;
+    $weeks = [
+        'Vorige week' => Week::current()->previous,
+        'Deze week' => Week::current(),
+        'Volgende week' => Week::current()->next,
+    ]
+@endphp
+
 @section('title', 'Dashboard')
 
 @section('content')
@@ -20,75 +29,35 @@
             </tr>
             </thead>
             <tbody>
-            <tr>
-                <td class="pr-2">
-                    Vorige week<br/>
-                    <small class="font-sm">{{\App\Models\Week::current()->previous->label}}</small>
-                </td>
-                @if(\App\Models\Week::current()->previous->is_holiday)
-                    <td
-                        class="text-center bg-gray-100"
-                        colspan="{{\App\Models\User::count()}}"
-                    >
-                        🎉 Vakantie! 🎉
+            @foreach($weeks as $label => $week)
+                <tr>
+                    <td class="pr-2">
+                        {{$label}}<br/>
+                        <small class="font-sm">{{$week->label}}</small>
                     </td>
-                @else
-                    @foreach(\App\Models\User::all() as $user)
-                        <td @class([
-                            "p-2",
-                            "bg-blue-100" => $user->id === auth()->id()
-                        ])>
-                            {{$user->assignmentForWeek(\App\Models\Week::current()->previous)?->task->name ?? 'Geen taak'}}
+                    @if($week->is_holiday)
+                        <td
+                            class="bg-gray-100"
+                            colspan="{{\App\Models\User::count()}}"
+                        >
+                            <div class="flex flex-row justify-center space-x-3">
+                                <span>🎉</span>
+                                <span>Vakantie!</span>
+                                <span>🎉</span>
+                            </div>
                         </td>
-                    @endforeach
-                @endif
-            </tr>
-            <tr>
-                <td class="pr-2">
-                    Deze week<br/>
-                    <small class="font-sm">{{\App\Models\Week::current()->label}}</small>
-                </td>
-                @if(\App\Models\Week::current()->is_holiday)
-                    <td
-                        class="text-center bg-gray-100"
-                        colspan="{{\App\Models\User::count()}}"
-                    >
-                        🎉 Vakantie! 🎉
-                    </td>
-                @else
-                    @foreach(\App\Models\User::all() as $user)
-                        <td @class([
-                            "p-2",
-                            "bg-blue-100" => $user->id === auth()->id()
-                        ])>
-                            {{$user->assignmentForWeek(\App\Models\Week::current())?->task->name ?? 'Geen taak'}}
-                        </td>
-                    @endforeach
-                @endif
-            </tr>
-            <tr>
-                <td class="pr-2">
-                    Volgende week<br/>
-                    <small class="font-sm">{{\App\Models\Week::current()->next->label}}</small>
-                </td>
-                @if(\App\Models\Week::current()->next->is_holiday)
-                    <td
-                        class="text-center bg-gray-100"
-                        colspan="{{\App\Models\User::count()}}"
-                    >
-                        🎉 Vakantie! 🎉
-                    </td>
-                @else
-                    @foreach(\App\Models\User::all() as $user)
-                        <td @class([
-                            "p-2",
-                            "bg-blue-100" => $user->id === auth()->id()
-                        ])>
-                            {{$user->assignmentForWeek(\App\Models\Week::current()->next)?->task->name ?? 'Geen taak'}}
-                        </td>
-                    @endforeach
-                @endif
-            </tr>
+                    @else
+                        @foreach(\App\Models\User::all() as $user)
+                            <td @class([
+                                "p-2",
+                                "bg-blue-100" => $user->id === auth()->id()
+                            ])>
+                                {{$user->assignmentForWeek($week)?->task->name ?? 'Geen taak'}}
+                            </td>
+                        @endforeach
+                    @endif
+                </tr>
+            @endforeach
             </tbody>
         </table>
     </div>
